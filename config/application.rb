97316d599121ff73a720983c4028aa1e72c40281
Yoshida-yuki <delicious-a@ezweb.ne.jp>
2020-09-30 18:57:54 +0900

    不要なファイルの削除

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Recipi
+module CookpadAppDemo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
@@ -15,6 +15,6 @@ module Recipi
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
   end
 end
-config.assets.initialize_on_precompile = false
